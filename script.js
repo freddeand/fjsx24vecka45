2710ea7ee7a7b665b8f8abca6050ff988f463ffe
Mandus Lindström <mandus85@hotmail.com>
2024-11-05 09:56:11 +0100

    Done with code along part 1, time for exercises

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 console.log("Godmorgon FJSX24 det är tisdag.");
 
 // getElementById
+// let headLine = document.getElementById("rubrik");
 
 // getElementsByClassName
+// let paragraf = document.getElementsByClassName("text");
 
 // querySelector och querySelectorAll
+// let firstListItem = document.querySelector(".item");
+// let allListItems = document.querySelectorAll(".item");
+// console.log("allListItems: ", allListItems);
 
-// PAUS
 // textContent vs. innerHTML
+// headLine.textContent = "Hej Sverige";
+// headLine.innerHTML += `<li class="item" >Fjärde</li>`;
 
-// Ändra Attribut
+// Ändra Attribut med setAttribute
+// let catImage = document.getElementById("bild");
 
-// Ändra Stilar
+// catImage.setAttribute("src", "bild2.jpg");
+// catImage.setAttribute("alt", "cat that needs to go to the dentist");
+
+// Ändra Stilar med style
+// headLine.style.color = "blue";
+// headLine.style.fontSize = "4rem";
 
 // Använda classList för att Hantera Klasser
+// let btn = document.getElementById("knapp");
+// btn.classList.add("addClass");
+// btn.classList.remove("addClass");
+// btn.classList.add("beforeToggle");
+// btn.classList.toggle("beforeToggle");
+// btn.classList.toggle("beforeToggle2222");
 
+// console.log("btn: ", btn);
+// PAUS
 // --------------------------------
 
 // Eventuellt vid tid:
